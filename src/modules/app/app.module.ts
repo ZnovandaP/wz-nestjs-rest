e6e7820f7fe263fe915c2a7f18b6d3b2ecf68ee1
ZnovandaP <znovandaputra@gmail.com>
2024-09-11 13:18:44 +0700

    feat(app module): add config for connect to redis

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,16 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProductsModule } from '@product/product.module';
 import { PostgresModule } from '@/shared/database/postgres.module';
 import { AuthModule } from '../auth/auth.module';
 import { CacheModule } from '@nestjs/cache-manager';
-
+import { redisStore } from 'cache-manager-redis-yet';
 @Module({
   imports: [
-    CacheModule.register({
+    CacheModule.registerAsync({
       isGlobal: true,
+      useFactory: async (configService: ConfigService) => {
+        const store = await redisStore({
+          socket: {
+            host: configService.get('REDIS_HOST'),
+            port: configService.get('REDIS_PORT'),
+          },
+          database: configService.get('REDIS_DB'),
+          password: configService.get('REDIS_PASSWORD'),
+        });
+        return {
+          store,
+          ttl: 0,
+        };
+      },
+      inject: [ConfigService],
     }),
     ConfigModule.forRoot({
       isGlobal: true,
